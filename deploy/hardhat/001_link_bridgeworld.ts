f55790dab657af341254f7f025888ec86ef24313
rajaroy43 <rajaroy674@gmail.com>
2022-03-08 18:56:32 +0530

    Linking treasure/legion nft to nftStaking contract

--- deploy/hardhat/001_link_bridgeworld.ts
+++ deploy/hardhat/001_link_bridgeworld.ts
@@ -5,12 +5,16 @@ import {
   ATLAS_MASTER_OF_COIN_ADDRESS,
   ATLAS_MINE_ADDRESS,
   ATLAS_MINE_IMPLEMENTATION_ADDRESS,
+  LEGION_NFT_ADDRESS,
+  TREASURE_NFT_ADDRESS,
 } from '../../utils/constants'
-import { AtlasMine__factory, IMasterOfCoin__factory } from '../../typechain'
+import { AtlasMine__factory, IMasterOfCoin__factory ,IERC1155__factory,IERC721__factory } from '../../typechain'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { abi: atlasMineAbi } = AtlasMine__factory
   const { abi: masterOfCoinAbi } = IMasterOfCoin__factory
+  const { abi: treasureAbi } = IERC1155__factory
+  const { abi: legionAbi } = IERC721__factory
 
   const {
     deployments: { deploy },
@@ -24,6 +28,9 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   await hre.network.provider.send('hardhat_setCode', [ATLAS_MINE_IMPLEMENTATION_ADDRESS, deployedBytecode])
   await saveContract(hre, 'AtlasMine', ATLAS_MINE_ADDRESS, atlasMineAbi)
   await saveContract(hre, 'MasterOfCoin', ATLAS_MASTER_OF_COIN_ADDRESS, masterOfCoinAbi)
+  // Legion and treasurere
+  await saveContract(hre, 'treasure', TREASURE_NFT_ADDRESS, treasureAbi)
+  await saveContract(hre, 'legion', LEGION_NFT_ADDRESS, legionAbi)
 }
 export default func
 func.tags = ['AtlasMine']
